6afe7615fb2018d94e7f9e8cedd28de73bc43ef7
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-11 01:56:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -35,7 +35,7 @@ class CashRegister
     end
 
     def void_last_transaction
-      @total -= @last_transaction_amount
+      @ total = @total - @last_transaction_amount
       #  if @items.clear
       #    @total
         # binding.pry
